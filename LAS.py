4a42878047f6bcde8b8f8bd002d791f69b0b0742
haakooto <hakonolav.torvik@gmail.com>
2019-12-22 00:45:11 +0100

    restart, now variables are great to use

--- LAS.py
+++ LAS.py
@@ -3,59 +3,141 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import time
-import re
 
 
-class LAS_func:
+class Variables:
+    """
+    Class for holding variables in LAS-functions in dict
+
+    keys are name of variable; str
+    values are lambda-function with no argument,
+        returning value; int, float, ndarray, None
+    """
+
+    def __init__(self):
+        self.dictionary = {}
+        self.vars = self.dictionary
+
+    def insert(self, name, value):
+        """ Make sure types are right, then add lambda-funct to dict """
+        if not isinstance(name, str):
+            raise TypeError(
+                f"variable names must be type 'str', not type '{type(name)}'"
+            )
+
+        if isinstance(value, list):
+            value = np.asarray(value, dtype=float)
+
+        if not isinstance(value, (int, float, np.ndarray)) and value is not None:
+            raise TypeError(
+                f"variable value must be number(s) or None, not type '{type(name)}'"
+            )
+
+        self.vars[name] = lambda: value
+
+    def __call__(self, name):
+        """
+        Call by (), lambda triggered
+        """
+        try:
+            return self.vars[name]()
+        except KeyError:
+            f"variable {name} not defined in fuction"
+
+    def __getitem__(self, name):
+        """
+        Call by [], lambda not triggered
+        """
+        try:
+            return self.vars[name]
+        except KeyError:
+            f"variable {name} not defined in fuction"
+
+    def __str__(self):
+        S = ""
+        for i in self.vars:
+            S += f"{i}: {self(i)}\n"
+        return S
+
+
+class LAS:
     def __init__(self, expression):
-        self.original = str(expression)
-        self.function = []
-        self.variables = {}
+        if not isinstance(expression, str):
+            raise TypeError(
+                f"given expression must be 'str', not '{type(expression)}''"
+            )
 
-        self.interpret(self.original)
+        self.original = expression
+        self.vars = Variables()
 
-    def interpret(self, expr):
-        if "=" in expr:
-            expr = self._defined_function(expr)
+        self.haupt = lambda: np.asarray(
+            [
+                list(self.vars.vars.items())[-i - 1][0]
+                if list(self.vars.vars.items())[-i - 1][1]() is None
+                else None
+                for i in range(len(self.vars.vars))
+            ]
+        )[
+            np.where(
+                np.asarray(
+                    [
+                        list(self.vars.vars.items())[-i - 1][0]
+                        if list(self.vars.vars.items())[-i - 1][1]() is None
+                        else None
+                        for i in range(len(self.vars.vars))
+                    ]
+                )
+            )
+        ][
+            0
+        ]
 
-        expr_array = np.asarray([s for s in expr], dtype=str)
-        left_paranthesis = np.where(expr_array == "(")
-        right_paranthesis = np.where(expr_array == ")")
+        self.variables_given = False
+        self.interpreted = False
 
-        self._parenthesis_count(left_paranthesis, right_paranthesis)
+    def decleare_variables(self, vars):
+        self._add_variables(vars)
+        self.variables_given = True
 
-        assert left_paranthesis.shape >= right_paranthesis.shape, "Too many "
+    def set_variable(self, name, value):
+        self.vars.insert(name, value)
 
-        # print(expr.find("("))
-        for m in re.finditer("(", expr):
-            print(m)
-        # p = [m.start() for m in re.finditer("(", str(expr))]
-        # print(p)
-        # expr = np.asarray(expr, dtype=str)
-        # print(expr)
-        # print(np.where(expr == "2"))
+    def unset_variable(self, name):
+        self.vars.insert(name, None)
 
-    def _defined_function(self, expr):
-        # if user has defined, such as f(x) = something
-        # can use this to find main variable(s)
-        # for now, just return what's after =
-        return expr[expr.find("=") + 1 :]
+    def _add_variables(self, vars):
+        if isinstance(vars, dict):
+            for key, value in vars.items():
+                self.vars.insert(key, value)
 
-    def _parenthesis_count(left, right):
-        l = len(left)
-        r = len(right)
-        if l > r:
-            self = self.original + ")" * (l - r)
-        elif 
+        elif isinstance(vars, (list, tuple)):
+            for var in vars:
+                self.vars.insert(var, None)
+
+        elif isinstance(vars, str):
+            vars = vars.split(", ")
+            for var in vars:
+                self.vars.insert(var, None)
+
+        elif isinstance(vars, (int, float)):
+            raise TypeError("Variable names must be str")
 
 
 def main():
-    """
-    input: "f(t)=Vp + vmax*sin((2pi/P)(t-t0))"
-    output:
-        ["add", Vp, ["mul", vmax, [sin, ["mul", ["div", ["mul", 2, pi], P], ["sub", t, t0]]]]]
-    """
-    f = LAS_func("f(t) = Vp + vmax*sin((2pi/P)(t-t0))")
+    # L = Variables()
+    # L.insert("b", None)
+    # print(L("b"))
+    # L.insert("b", 14)
+    # print(L["b"]())
+
+    F = LAS("ax+b")
+    F.decleare_variables({"x": np.arange(3), "y": None, "a": 1, "b": 4})
+    print(F.vars)
+    print(F.haupt())
+
+    F.decleare_variables("x, y, z")
+    print(F.vars)
+    print(F.haupt())
 
 
 if __name__ == "__main__":
