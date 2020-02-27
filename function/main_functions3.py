f440d4c8bb3328263f9bd454fd49752d2abb1fef
haakooto <hakonolav.torvik@gmail.com>
2020-02-27 23:16:10 +0100

    PEP8-ed

--- function/main_functions3.py
+++ function/main_functions3.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numbers import Number as number
 
+
 def listed_nest_remover(l):
     nested_list_dummy = []
 
@@ -14,14 +15,15 @@ def listed_nest_remover(l):
     list_nested_remover_2(l)
     return nested_list_dummy
 
+
 class Variable:
     def __init__(self, *args):
         pass
 
+
 class parentFunction:
     def __init__(self, *init_structure):
         self.init_structure = init_structure
-
 
     def __call__(self, *args):
         call_arg = args[0]
@@ -29,9 +31,11 @@ class parentFunction:
         if isinstance(call_arg, Variable):
             return self
         elif isinstance(call_arg, number):
-            assert len(args) == 1, 'Only takes 1 input'
+            assert len(args) == 1, "Only takes 1 input"
             call_arg = args[0]
-            init_structure_variables_replaced = self.replace_variables_with_number(call_arg)
+            init_structure_variables_replaced = self.replace_variables_with_number(
+                call_arg
+            )
 
             return self.call(init_structure_variables_replaced)
 
@@ -49,18 +53,21 @@ class parentFunction:
         return init_structure_variables_replaced
 
 
-
 class add(parentFunction):
     def call(self, *args):
         res = 0
         for obj in args[0]:
             res += obj
         return res
+
+
 class sub(parentFunction):
     def call(self, *args):
         args = args[0]
-        assert len(args) == 2, 'sub takes two arguments a,b -> a - b'
-        return args[0]- args[1]
+        assert len(args) == 2, "sub takes two arguments a,b -> a - b"
+        return args[0] - args[1]
+
+
 class mul(parentFunction):
     def call(self, *args):
         args = args[0]
@@ -68,60 +75,75 @@ class mul(parentFunction):
         for obj in args:
             res *= obj
         return res
+
+
 class div(parentFunction):
     def call(self, *args):
         args = args[0]
-        print(args)
-        assert len(args) == 2, 'div takes two arguments a,b -> a/b'
-        return args[0]/args[1]
+        assert len(args) == 2, "div takes two arguments a,b -> a/b"
+        return args[0] / args[1]
+
+
 class cos(parentFunction):
     def call(self, *args):
         arg = args[0]
-        assert len(arg) == 1, 'cos takes 1 argument a -> cos(a)'
+        assert len(arg) == 1, "cos takes 1 argument a -> cos(a)"
         return np.cos(arg[0])
+
+
 class sin(parentFunction):
     def call(self, *args):
         arg = args[0]
-        assert len(arg) == 1, 'sin takes 1 argument a -> sin(a)'
+        assert len(arg) == 1, "sin takes 1 argument a -> sin(a)"
         return np.sin(arg[0])
+
+
 class ln(parentFunction):
     def call(self, *args):
         arg = args[0]
-        assert len(arg) == 1, 'ln takes 1 argument a -> ln(a)'
+        assert len(arg) == 1, "ln takes 1 argument a -> ln(a)"
         return np.log(arg[0])
+
+
 class log(parentFunction):
     def call(self, *args):
         arg = args[0]
-        assert len(arg) == 1, 'log takes 1 argument a -> log(a)'
+        assert len(arg) == 1, "log takes 1 argument a -> log(a)"
         return np.log10(arg[0])
+
+
 class pow(parentFunction):
     def call(self, *args):
         args = args[0]
-        assert len(args) == 2, 'pow takes two arguments a,b -> a^b'
+        assert len(args) == 2, "pow takes two arguments a,b -> a^b"
         try:
-            return args[0]**args[1]
+            return args[0] ** args[1]
         except RuntimeWarning:
-            print('Imaginary numbers not yet supported')
+            print("Imaginary numbers not yet supported")
             import sys
+
             sys.exit(1)
+
+
 class sqrt(parentFunction):
     def call(self, *args):
         args = args[0]
-        assert len(args) == 1, 'pow takes one argument a -> sqrt(a)'
+        assert len(args) == 1, "pow takes one argument a -> sqrt(a)"
         try:
-            return args[0]**(1/2)
+            return args[0] ** (1 / 2)
         except RuntimeWarning:
-            print('Imaginary numbers not yet supported')
+            print("Imaginary numbers not yet supported")
             import sys
+
             sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     x = Variable()
     a = cos(x)
-    b= sin(x)
+    b = sin(x)
     s = sin(x)
     c = cos(x)
-    tan = div(sin(x),cos(x))
-    t = sqrt(add(1,mul(-1,tan(x))))
+    tan = div(sin(x), cos(x))
+    t = sqrt(add(1, mul(-1, tan(x))))
     print(t(2))
