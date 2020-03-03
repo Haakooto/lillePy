3e397ce610cb35ebbfc2ee2271679bd095d07e28
haakooto <hakonolav.torvik@gmail.com>
2020-03-03 23:10:50 +0100

    remove trash

--- function/__init__.py
+++ function/__init__.py
@@ -1,12 +1,4 @@
-# from . import Variable, parentFunction, main_functions, function_compacting
-# from . import Variable
 from .Variable import *
-
-# from . import parentFunction
 from .parentFunction import *
-
-# from . import main_functions
 from .main_functions import *
-
-# from . import function_compacting
 from .function_compacting import *
