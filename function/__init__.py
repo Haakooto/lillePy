f0f3dde7b3515e30745a73538397e43b14bb2834
bahjemga <bror.hjemgaard@gmail.com>
2020-03-06 18:52:47 +0100

    added the names of the renamed files

--- function/__init__.py
+++ function/__init__.py
@@ -1,6 +1,6 @@
 from .Variable import *
-from .parentFunction import *
-from .main_functions import *
-from .parentOperator import *
+from .parents import *
+from .basicFunctions import *
+from .operatorFunctions import *
 
 # from .function_compacting import *
