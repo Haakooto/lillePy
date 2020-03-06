92d9da31970069c957957d5719f2dd9d484b3332
bahjemga <bror.hjemgaard@gmail.com>
2020-03-06 18:04:07 +0100

    this is a module that allwos for the convertion from normal string to super and subscript string

---
+++ function/subandsuperscript.py
@@ -0,0 +1,18 @@
+
+def superscript(input):
+    # this only supports numbers for now. when letter support is aded, the below
+    # try-except can be removed
+    try:
+        float(input)
+    except:
+        print('superscript for now only takes numbers!')
+        import sys
+        sys.exit([1])
+
+    #assert isinstance(input, String), f'Expected type string got {type(input)}'
+
+    superscriptdict = {'1':'¹', '2':'²', '3':'³', '4':'⁴', '5':'⁵', '6':'⁶', '7':'⁷', '8':'⁸', '9':'⁹', '0':'⁰'}
+    res = ''
+    for i in input:
+        res += superscriptdict[i]
+    return res
