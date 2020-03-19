c1be9e34551927b73aafcdfb6d3a03f4a1077691
bahjemga <bror.hjemgaard@gmail.com>
2020-03-19 13:55:36 +0100

    added support for import of locals from any python script

--- __init__.py
+++ __init__.py
@@ -6,16 +6,33 @@ import CallableModules
 
 
 import sys
-import os
+from fabric.api import local
 
-sys.path.append(sys.argv[0])
-print(os.getcwd(), sys.argv[0])
-scriptname = sys.argv[0]
-if scriptname[-3:] == ".py":
-    scriptname = scriptname[:-3]
-print(scriptname)
-exec(f"from {scriptname} import *")
-print(locals()["aa"])
+print(local("aa"))
+print(sys.argv[0], "sys.argv")
+argv = sys.argv[0]
+
+if argv[-9:] == "/ipython3":
+    index = argv.rindex("/")
+    filename = argv[index + 1 :]
+    filepath = argv[:index]
+    print(filename, filepath)
+    sys.path.append(filename)
+    exec(f"from {filename} import *")
+
+else:
+    # sys.path.append(sys.argv[0])
+    # print(os.getcwd(), sys.argv)
+    scriptname = sys.argv[0]
+    if scriptname[-3:] == ".py":
+        scriptname = scriptname[:-3]
+    elif scriptname[-9:] == "/ipython3":
+        scriptname = scriptname[:-9]
+
+    sys.path.append(scriptname)
+    print(scriptname, "yo")
+    exec(f"from {scriptname} import *")
+    print(locals()["aa"])
 
 
 global x
